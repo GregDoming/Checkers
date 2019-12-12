0f93f89ebd9e70cc313c8815ba565f121d672b63
Gregory <greg.domingue1@gmail.com>
2019-12-12 12:02:30 -0800

    made board container.

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Cell from "../Cell/Cell";
+import "./Board.css";
+
+const Board = () => {
+  return <div className="BoardContainer"></div>;
+};
+
+export default Board;
